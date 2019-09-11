46d3cd7867c3a69ca6bd8b99300ae016039aca3b
apierro <antonio.pierro@gmail.com>
2019-09-11 11:52:46 +0200

    Adding getShippingPrices that uses the HttpClient#get() method

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
     providedIn: "root"
 })
 export class CartService {
-    constructor() {}
+    constructor(private http: HttpClient) {}
     items = [];
 
     addToCart(product) {
@@ -18,5 +19,9 @@ export class CartService {
     clearCart() {
         this.items = [];
         return this.items;
+    }
+
+    getShippingPrices() {
+        return this.http.get("/assets/shipping.json");
     }
 }
