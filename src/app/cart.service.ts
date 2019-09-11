0ab15ef1d0a560bd5d338680ad2baa1c99cc8fc4
apierro <antonio.pierro@gmail.com>
2019-09-11 10:34:23 +0200

    Defining methods to add items to the cart, return cart items, and clear the cart items

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -5,4 +5,18 @@ import { Injectable } from "@angular/core";
 })
 export class CartService {
     constructor() {}
+    items = [];
+
+    addToCart(product) {
+        this.items.push(product);
+    }
+
+    getItems() {
+        return this.items;
+    }
+
+    clearCart() {
+        this.items = [];
+        return this.items;
+    }
 }
